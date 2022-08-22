d44b654c82111a70d62cddf03aba60b3e4e6b0d8
rezaalp <rezaalp05@google.com>
2022-08-22 11:43:30 +0700

    test

--- boilerplate/src/App.js
+++ boilerplate/src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+
+    making a boilerplate
         </a>
       </header>
     </div>
